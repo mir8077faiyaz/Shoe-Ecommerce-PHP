33816aec071802e103e2e0d58bd66a8436342bba
Mir Faiyaz Hossain <63485441+mir8077faiyaz@users.noreply.github.com>
2023-12-06 02:41:58 +0600

    Fixed Navbar

--- Abibas/php/nav.php
+++ Abibas/php/nav.php
@@ -1,28 +1,71 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark "> 
-        <div>
-            <a class="navbar-brand ml-3" href="home.php">
-                <img src="../images/logo.jpg" alt="..." height="60">
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-        </div>
-      
-        <div class="collapse navbar-collapse mr-5" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto"> 
-            <li class="nav-item mx-2">
-              <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item mx-2">
-              <a class="nav-link" href="login.php">Login</a>
-            </li>
-          </ul>
-          <ul class="navbar-nav">
-            <li class="nav-item mx-2">
-                <a class="navbar-brand" href="checkout.php">
-                    <img src="../images/cart.png" alt="..." height="30">
-                </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+<?php
+if(isset($_SESSION['login_id'])){
+  echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark ">';
+  echo '    <div>';
+  echo '        <a class="navbar-brand ml-3" href="home.php">';
+  echo '            <img src="../images/logo.jpg" alt="..." height="60">';
+  echo '        </a>';
+  echo '        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">';
+  echo '          <span class="navbar-toggler-icon"></span>';
+  echo '        </button>';
+  echo '    </div>';
+  echo '';
+  echo '    <div class="collapse navbar-collapse mr-5" id="navbarSupportedContent">';
+  echo '        <ul class="navbar-nav ml-auto">';
+  echo '            <li class="nav-item mx-2">';
+  echo '                <a class="navbar-brand" href="home.php">'; // To be updated to user-Panel
+  echo '                    <img src="' . $user['profile_image'] . '" alt="..." height="30">';
+  echo '                </a>';
+  echo '            </li>';
+  echo '            <li class="nav-item mx-2">';
+  echo '                <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>';
+  echo '            </li>';
+  echo '            <li class="nav-item mx-2">';
+  echo '                <a class="nav-link" href="logout.php">Logout</a>';
+  echo '            </li>';
+  echo '        </ul>';
+  echo '        <ul class="navbar-nav">';
+  echo '            <li class="nav-item mx-2">';
+  echo '                <a class="navbar-brand" href="checkout.php">';
+  echo '                    <img src="../images/cart.png" alt="..." height="30">';
+  echo '                </a>';
+  echo '            </li>';
+  echo '        </ul>';
+  echo '    </div>';
+  echo '</nav>';
+
+}
+else{
+  echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark ">';
+  echo '    <div>';
+  echo '        <a class="navbar-brand ml-3" href="home.php">';
+  echo '            <img src="../images/logo.jpg" alt="..." height="60">';
+  echo '        </a>';
+  echo '        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">';
+  echo '          <span class="navbar-toggler-icon"></span>';
+  echo '        </button>';
+  echo '    </div>';
+  echo '';
+  echo '    <div class="collapse navbar-collapse mr-5" id="navbarSupportedContent">';
+  echo '        <ul class="navbar-nav ml-auto">';
+  echo '            <li class="nav-item mx-2">';
+  echo '                <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>';
+  echo '            </li>';
+  echo '            <li class="nav-item mx-2">';
+  echo '                <a class="nav-link" href="login.php">Login</a>';
+  echo '            </li>';
+  echo '        </ul>';
+  echo '        <ul class="navbar-nav">';
+  echo '            <li class="nav-item mx-2">';
+  echo '                <a class="navbar-brand" href="checkout.php">';
+  echo '                    <img src="../images/cart.png" alt="..." height="30">';
+  echo '                </a>';
+  echo '            </li>';
+  echo '        </ul>';
+  echo '    </div>';
+  echo '</nav>';
+}
+?>
+
+
+
