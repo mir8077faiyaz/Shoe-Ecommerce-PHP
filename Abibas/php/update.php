4202aced3f095c065a6c8fd95f4c20adf7efadcb
Mir Faiyaz Hossain <63485441+mir8077faiyaz@users.noreply.github.com>
2023-12-12 01:35:39 +0600

    Update page fixed

--- Abibas/php/update.php
+++ Abibas/php/update.php
@@ -12,52 +12,53 @@ $desc=$row['description'];
 $img=$row['image'];
 
 
-<script>
-        $(":checkbox").prop('checked', false).parent().removeClass('active');
-</script>
-<form class="mx-5" method="post" action=''>
-    <div class="form-group">
-        <label for="name">Name</label>
-        <input type="text" class="form-control" id="name" placeholder="Enter name" value='$name' name="name">
-    </div>
+echo '<script>';
+echo '    $(":checkbox").prop(\'checked\', false).parent().removeClass(\'active\');';
+echo '</script>';
 
-    <div class="form-group">
-        <label for="price">Price</label>
-        <input type="number" class="form-control" id="price" placeholder="Price" name="price">
-    </div>
-    <p>Select Size:</p>
+echo '<form class="mx-5" method="post" action="">';
+echo '    <div class="form-group">';
+echo '        <label for="name">Name</label>';
+echo '        <input type="text" class="form-control" id="name" placeholder="Enter name" value=\'$name\' name="name">';
+echo '    </div>';
 
-    <div class="form-check-inline">
-        <label class="form-check-label mr-2" for="check1">
-            <input type="checkbox" class="form-check-input" id="check1" name="options[]" value="4" checked>4
-        </label>
-        <label class="form-check-label mx-2" for="check2">
-            <input type="checkbox" class="form-check-input" id="check2" name="options[]" value="5" checked>5
-        </label>
-        <label class="form-check-label mx-2" for="check3">
-            <input type="checkbox" class="form-check-input" id="check3" name="options[]" value="6" checked>6
-        </label>
-        <label class="form-check-label mx-2" for="check4">
-            <input type="checkbox" class="form-check-input" id="check4" name="options[]" value="7" checked>7
-        </label>
-        <label class="form-check-label mx-2" for="check5">
-            <input type="checkbox" class="form-check-input" id="check5" name="options[]" value="8" checked>8
-        </label>
-        <label class="form-check-label mx-2" for="check6">
-            <input type="checkbox" class="form-check-input" id="check6" name="options[]" value="9" checked>9
-        </label>
-    </div>
+echo '    <div class="form-group">';
+echo '        <label for="price">Price</label>';
+echo '        <input type="number" class="form-control" id="price" placeholder="Price" name="price">';
+echo '    </div>';
+echo '    <p>Select Size:</p>';
 
-    <div class="form-group">
-        <label for="desc">Description</label>
-        <textarea class="form-control" id="desc" rows="3" name="desc" placeholder="Description"></textarea>
-    </div>
+echo '    <div class="form-check-inline">';
+echo '        <label class="form-check-label mr-2" for="check1">';
+echo '            <input type="checkbox" class="form-check-input" id="check1" name="options[]" value="4" checked>4';
+echo '        </label>';
+echo '        <label class="form-check-label mx-2" for="check2">';
+echo '            <input type="checkbox" class="form-check-input" id="check2" name="options[]" value="5" checked>5';
+echo '        </label>';
+echo '        <label class="form-check-label mx-2" for="check3">';
+echo '            <input type="checkbox" class="form-check-input" id="check3" name="options[]" value="6" checked>6';
+echo '        </label>';
+echo '        <label class="form-check-label mx-2" for="check4">';
+echo '            <input type="checkbox" class="form-check-input" id="check4" name="options[]" value="7" checked>7';
+echo '        </label>';
+echo '        <label class="form-check-label mx-2" for="check5">';
+echo '            <input type="checkbox" class="form-check-input" id="check5" name="options[]" value="8" checked>8';
+echo '        </label>';
+echo '        <label class="form-check-label mx-2" for="check6">';
+echo '            <input type="checkbox" class="form-check-input" id="check6" name="options[]" value="9" checked>9';
+echo '        </label>';
+echo '    </div>';
 
-    <div class="form-group">
-        <label for="img">Image</label>
-        <input type="file" class=" form-control btn-sm pb-3" id="img" placeholder="Image" name="img">
-    </div>
-    <button type="submit" name="submit" class="btn btn-primary">Add Product</button>
-</form>
+echo '    <div class="form-group">';
+echo '        <label for="desc">Description</label>';
+echo '        <textarea class="form-control" id="desc" rows="3" name="desc" placeholder="Description"></textarea>';
+echo '    </div>';
+
+echo '    <div class="form-group">';
+echo '        <label for="img">Image</label>';
+echo '        <input type="file" class=" form-control btn-sm pb-3" id="img" placeholder="Image" name="img">';
+echo '    </div>';
+echo '    <button type="submit" name="submit" class="btn btn-primary">Add Product</button>';
+echo '</form>';
 
 ?>
